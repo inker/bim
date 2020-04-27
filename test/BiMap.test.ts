db86d6725c5e35344703f05187374d03acbc16ba
inker <anton.veselyev@gmail.com>
2020-04-27 16:57:20 +0100

    update tests

---
+++ test/BiMap.test.ts
@@ -0,0 +1,201 @@
+import { BiMap } from '../src'
+
+describe('BiMap', () => {
+  it('should be empty at the start', () => {
+    const m = new BiMap()
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should set correctly', () => {
+    const m = new BiMap<string, number>()
+    m.set('foo', 3)
+    m.set('bar', 5)
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should accept array of entries', () => {
+    const prodedural = new BiMap()
+    prodedural.set('foo', 3)
+    prodedural.set('bar', 5)
+
+    const fromEntries = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect(fromEntries).toMatchSnapshot()
+    expect(fromEntries).toEqual(prodedural)
+  })
+
+  it('should delete key correctly', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    m.delete('foo')
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should should ignore non-existing keys', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    m.delete('qux')
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should delete value correctly', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    m.deleteValue(3)
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should should ignore non-existing values', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    m.deleteValue(-1)
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should change value', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    m.set('foo', -1)
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should should have working get', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect(m.get('foo')).toBe(3)
+    expect(m.getKey(3)).toBe('foo')
+
+    expect(m.get('qux')).toBe(undefined)
+  })
+
+  it('should should have working has', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect(m.has('foo')).toBe(true)
+    expect(m.hasValue(3)).toBe(true)
+
+    expect(m.has('qux')).toBe(false)
+    expect(m.hasValue(-1)).toBe(false)
+  })
+
+  it('should get keys', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect([...m.keys()]).toMatchSnapshot()
+  })
+
+  it('should get values', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect([...m.values()]).toMatchSnapshot()
+  })
+
+  it('should get entries', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect([...m.entries()]).toMatchSnapshot()
+  })
+
+  it('should get entries by spread', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect([...m]).toMatchSnapshot()
+  })
+
+  it('should iterate keys', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    const arr = []
+    for (const key of m.keys()) {
+      arr.push(key)
+    }
+    expect(arr).toMatchSnapshot()
+  })
+
+  it('should iterate values', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    const arr = []
+    for (const val of m.values()) {
+      arr.push(val)
+    }
+    expect(arr).toMatchSnapshot()
+  })
+
+  it('should iterate entries', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    const arr = []
+    for (const pair of m.entries()) {
+      arr.push(pair)
+    }
+    expect(arr).toMatchSnapshot()
+  })
+
+  it('should iterate entries (without .entries())', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    const arr = []
+    for (const pair of m) {
+      arr.push(pair)
+    }
+    expect(arr).toMatchSnapshot()
+  })
+
+  it('should return correct size', () => {
+    const m = new BiMap([
+      ['foo', 3],
+      ['bar', 5],
+    ])
+
+    expect(m.size).toBe(2)
+  })
+})
---
+++ test/WeakBiMap.test.ts
@@ -0,0 +1,105 @@
+import { WeakBiMap } from '../src'
+
+describe('WeakBiMap', () => {
+  it('should be empty at the start', () => {
+    const m = new WeakBiMap()
+    expect(m).toMatchSnapshot()
+  })
+
+  it('should get & delete key correctly', () => {
+    const a = { a: 1 }
+    const b = { b: 2 }
+    const c = { c: 3 }
+    const d = { d: 4 }
+    const m = new WeakBiMap<{ [key: string]: number }, { [key: string]: number }>([
+      [a, c],
+      [b, d],
+    ])
+
+    expect(m.get(a)).toBe(c)
+    expect(m.getKey(c)).toBe(a)
+    m.delete(a)
+    expect(m.get(a)).toBe(undefined)
+    expect(m.getKey(c)).toBe(undefined)
+    expect(m.get(b)).toBe(d)
+    expect(m.getKey(d)).toBe(b)
+  })
+
+  it('should get & delete value correctly', () => {
+    const a = { a: 1 }
+    const b = { b: 2 }
+    const c = { c: 3 }
+    const d = { d: 4 }
+    const m = new WeakBiMap<{ [key: string]: number }, { [key: string]: number }>([
+      [a, c],
+      [b, d],
+    ])
+
+    expect(m.get(a)).toBe(c)
+    expect(m.getKey(c)).toBe(a)
+    m.deleteValue(c)
+    expect(m.get(a)).toBe(undefined)
+    expect(m.getKey(c)).toBe(undefined)
+    expect(m.get(b)).toBe(d)
+    expect(m.getKey(d)).toBe(b)
+  })
+
+  it('should should ignore non-existing values', () => {
+    const a = { a: 1 }
+    const b = { b: 2 }
+    const c = { c: 3 }
+    const d = { d: 4 }
+    const m = new WeakBiMap<{ [key: string]: number }, { [key: string]: number }>([
+      [a, c],
+      [b, d],
+    ])
+
+    expect(m.get(a)).toBe(c)
+    expect(m.getKey(c)).toBe(a)
+    m.deleteValue({})
+    expect(m.get(a)).toBe(c)
+    expect(m.getKey(c)).toBe(a)
+    expect(m.get(b)).toBe(d)
+    expect(m.getKey(d)).toBe(b)
+  })
+
+  it('should change value', () => {
+    const a = { a: 1 }
+    const b = { b: 2 }
+    const c = { c: 3 }
+    const d = { d: 4 }
+    const m = new WeakBiMap<{ [key: string]: number }, { [key: string]: number }>([
+      [a, c],
+      [b, d],
+    ])
+
+    const e = { e: 5 }
+    m.set(a, e)
+    expect(m.get(a)).toBe(e)
+    expect(m.getKey(e)).toBe(a)
+    expect(m.getKey(c)).toBe(undefined)
+    expect(m.get(b)).toBe(d)
+    expect(m.getKey(d)).toBe(b)
+  })
+
+  it('should should have working has', () => {
+    const a = { a: 1 }
+    const b = { b: 2 }
+    const c = { c: 3 }
+    const d = { d: 4 }
+    const m = new WeakBiMap<{ [key: string]: number }, { [key: string]: number }>([
+      [a, c],
+      [b, d],
+    ])
+
+    const e = { e: 5 }
+    expect(m.has(a)).toBe(true)
+    expect(m.hasValue(c)).toBe(true)
+    m.set(a, e)
+    expect(m.has(a)).toBe(true)
+    expect(m.hasValue(e)).toBe(true)
+    expect(m.hasValue(c)).toBe(false)
+    expect(m.has(b)).toBe(true)
+    expect(m.hasValue(d)).toBe(true)
+  })
+})
